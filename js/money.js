1714710e8f20c4f3034b64c7e08e4f84e2935879
RitaM5 <sporshia108514orva@gmail.com>
2022-02-17 19:20:04 +0600

    more code added

--- js/money.js
+++ js/money.js
@@ -2,8 +2,9 @@
 document.getElementById('calculate-btn').addEventListener('click', function(){
 
    balance = mainCalculation();
-   document.getElementById('display-balance').innerText = balance;
-
+  if(balance) {
+      document.getElementById('display-balance').innerText = balance;
+    }
 });
 
 function inputCalculation(){
@@ -13,29 +14,36 @@ function inputCalculation(){
 }
 
 function mainCalculation(){
-    
     inputIncome = inputCalculation();
     let foodInputValue = document.getElementById('input-food-price') ;
     let foodValue = parseInt(foodInputValue.value);
+
     let rentInputValue = document.getElementById('input-rent-price');
     let rentValue = parseInt(rentInputValue.value);
+
     let clothesInputValue = document.getElementById('input-clothes-price');
     let clothesValue = parseInt(clothesInputValue.value);
 //error handle for inputField
-  if(isNaN(inputIncome)==true|| isNaN(rentValue)==true|| isNaN(foodValue)==true||isNaN(clothesValue)==true){
+  if(isNaN(inputIncome)|| isNaN(rentValue)|| isNaN(foodValue)||isNaN(clothesValue)){
     return alert("Please write a number !!");
   } 
   else if(inputIncome<0||foodValue<0||rentValue<0||clothesValue<0){
       return alert('Please write a positive number !!');
+      
   }   
-  //total expenses
-   else if(){
-       let totalExpenses = foodValue + rentValue + clothesValue ;
-       document.getElementById('total-expenses').innerText = totalExpenses;
+//total expenses and balance
+   
+     else {let totalExpenses = foodValue + rentValue + clothesValue ;
 
-       let balance = inputIncome - totalExpenses;
-       return balance;
-   }
+         if(inputIncome >= totalExpenses) {
+             document.getElementById('total-expenses').innerText = totalExpenses;
+           let balance = inputIncome - totalExpenses;
+           return balance;
+        }
+        else{
+            return alert('It cost more then your total money !');
+        }
+     }
 }
 
 //saving part
