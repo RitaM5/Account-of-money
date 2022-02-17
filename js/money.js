5113df13523d9ed6f9c066f829d9f20a7eda7ff8
RitaM5 <sporshia108514orva@gmail.com>
2022-02-17 15:51:25 +0600

    code added

--- js/money.js
+++ js/money.js
@@ -6,35 +6,38 @@ document.getElementById('calculate-btn').addEventListener('click', function(){
 
 });
 
-function mainCalculation(){
-    
+function inputCalculation(){
     let input= document.getElementById('input-income');
     let inputIncome = parseInt(input.value);
+     return inputIncome;
+}
+
+function mainCalculation(){
     //error handle for inputField
-   
-    let foodValue = document.getElementById('input-food-price') ;
-    let rentValue = document.getElementById('input-rent-price');
-    let clothesValue = document.getElementById('input-clothes-price');
+    inputIncome = inputCalculation();
+    let foodInputValue = document.getElementById('input-food-price') ;
+    let foodValue = parseInt(foodInputValue.value);
+    let rentInputValue = document.getElementById('input-rent-price');
+    let rentValue = parseInt(rentInputValue.value);
+    let clothesInputValue = document.getElementById('input-clothes-price');
+    let clothesValue = parseInt(clothesInputValue.value);
 
-    //total expenses
-    let totalExpenses = parseInt(foodValue.value)+ parseInt(rentValue.value) + parseInt(clothesValue.value);
+  if(inputIncome ==String|| rentValue==String|| foodValue==String||clothesValue==String){
+    return alert("Please write a number");
+  }    
+  //total expenses
+   else{
+       let totalExpenses = foodValue + rentValue + clothesValue ;
+       document.getElementById('total-expenses').innerText = totalExpenses;
 
-    document.getElementById('total-expenses').innerText = totalExpenses;
-    //input.value = '';
-    /*foodValue.value ='';
-    rentValue.value ='';
-    clothesValue.value ='';*/
-
-    //balance
-    let balance = (inputIncome  - totalExpenses);
-    return balance;
+       let balance = inputIncome - totalExpenses;
+       return balance;
+   }
 }
 
 //saving part
 function savingMoney(){
-    let input= document.getElementById('input-income');
-    let inputIncome = parseInt(input.value);
-
+    inputIncome = inputCalculation();
     let saveParcent = document.getElementById('save-parcent');
     let savingAmount = parseInt(saveParcent.value);
 
@@ -46,15 +49,12 @@ document.getElementById('saving-btn').addEventListener('click', function(){
  
     balance = mainCalculation();
     totalSavingAmount = savingMoney();
-     //saving part
 
     if(balance < totalSavingAmount){
-       alert('you dont have sufficient balance !!');
+       return alert('you dont have sufficient balance !!');
     }
-
     else {
         document.getElementById('saving-amount').innerText = totalSavingAmount;
     }
-
     document.getElementById('remaining-balance').innerText = balance - totalSavingAmount ;
 })
